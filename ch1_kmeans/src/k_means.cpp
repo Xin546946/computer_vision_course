57eac32bdba70df119aa9e5d411a8d7e35b88aed
Xin Jin <xinjin1109@gmail.com>
2020-10-30 14:06:14 +0100

    adjust code style

--- ch1_kmeans/src/k_means.cpp
+++ ch1_kmeans/src/k_means.cpp
@@ -1,53 +1,52 @@
 #include "k_means.h"
-#include <set>
-#include <random>
-#include <vector>
 #include <algorithm>
+#include <random>
+#include <set>
+#include <vector>
 
 static std::random_device rd;
 static std::mt19937 rng(rd());
 
 std::set<int> get_random_index(int max_idx, int n);
 
-float check_convergence(std::vector<Center> current_centers, std::vector<Center> last_centers);
+float check_convergence(std::vector<Center> current_centers,
+                        std::vector<Center> last_centers);
 
 float calc_square_dist(const std::array<float, 3>& arr1,
-                        const std::array<float, 3>& arr2)
-    {
-        return arr1[0] * arr2[0] + arr1[1] * arr2[1] + arr1[2] * arr2[2];
-    }
+                       const std::array<float, 3>& arr2) {
+    return arr1[0] * arr2[0] + arr1[1] * arr2[1] + arr1[2] * arr2[2];
+}
 
-Kmeans::Kmeans(cv::Mat img, const int k){
+Kmeans::Kmeans(cv::Mat img, const int k) {
     centers_.resize(k);
     last_centers_.resize(k);
     samples_.reserve(img.rows * img.cols);
 
-    for(int r = 0; r < img.rows; r++) {
+    for (int r = 0; r < img.rows; r++) {
         for (int c = 0; c < img.cols; c++) {
             std::array<float, 3> tmp_feature;
             for (int channel = 0; channel < 3; channel++) {
-                tmp_feature[channel] = img.at<cv::Vec3f>(r,c)[channel]; 
+                tmp_feature[channel] = img.at<cv::Vec3f>(r, c)[channel];
             }
-            samples_.emplace_back(tmp_feature, r, c, -1); 
+            samples_.emplace_back(tmp_feature, r, c, -1);
         }
     }
 }
 
-
-void Kmeans::run(int max_iteration, float smallest_convergence_rate){
+void Kmeans::run(int max_iteration, float smallest_convergence_rate) {
     initial_centers();
     int current_iter = 0;
-    while(is_terminate(current_iter, max_iteration, smallest_convergence_rate)){
+    while (
+        is_terminate(current_iter, max_iteration, smallest_convergence_rate)) {
         current_iter++;
         update_labels();
         update_centers();
-
     }
 }
 
-
-void Kmeans::initial_centers(){
-    std::set<int> random_idx = get_random_index(samples_.size() - 1, centers_.size());
+void Kmeans::initial_centers() {
+    std::set<int> random_idx =
+        get_random_index(samples_.size() - 1, centers_.size());
     int i_center = 0;
 
     for (auto index : random_idx) {
@@ -56,13 +55,13 @@ void Kmeans::initial_centers(){
     }
 }
 
-bool Kmeans::is_terminate(int current_iter, int max_iteration, float smallest_convergence_rate) const
-{
+bool Kmeans::is_terminate(int current_iter, int max_iteration,
+                          float smallest_convergence_rate) const {
     float convergence_rate = check_convergence(last_centers_, centers_);
-    if (current_iter == max_iteration || convergence_rate < smallest_convergence_rate)
-        return true; 
+    if (current_iter == max_iteration ||
+        convergence_rate < smallest_convergence_rate)
+        return true;
 }
-
 
 std::set<int> get_random_index(int max_idx, int n) {
     std::uniform_int_distribution<int> dist(1, max_idx + 1);
@@ -74,11 +73,13 @@ std::set<int> get_random_index(int max_idx, int n) {
     return random_idx;
 }
 
-float check_convergence(std::vector<Center> current_centers, std::vector<Center> last_centers)
-{
+float check_convergence(std::vector<Center> current_centers,
+                        std::vector<Center> last_centers) {
     float convergence_rate = 0;
-    for(int i_center=0; i_center < current_centers.size(); i_center++){
-        convergence_rate += calc_square_dist(current_centers[i_center].position_, last_centers[i_center].position_);    
+    for (int i_center = 0; i_center < current_centers.size(); i_center++) {
+        convergence_rate +=
+            calc_square_dist(current_centers[i_center].position_,
+                             last_centers[i_center].position_);
     }
     return convergence_rate;
 }
