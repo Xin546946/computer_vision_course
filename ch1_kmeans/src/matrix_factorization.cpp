21239abc02c5e7f66eaf45bd83716e71d071f891
zhaoran <zhaoran.wu1@gmail.com>
2020-11-03 15:42:22 +0100

    rename function in nmf

--- ch1_kmeans/src/matrix_factorization.cpp
+++ ch1_kmeans/src/matrix_factorization.cpp
@@ -5,56 +5,46 @@
 #include <Eigen/Dense>
 #include <Eigen/QR>
 
-void non_negative_matrix_factorization(Eigen::MatrixXf V, Eigen::MatrixXf H,
-                                       Eigen::MatrixXf W, int max_iteration,
-                                       int r);
+void non_negative_matrix_factorization(const Eigen::MatrixXf& V,
+                                       Eigen::MatrixXf& H, Eigen::MatrixXf& W,
+                                       int num_basis, int max_iteration);
 
 int main() {
-    Eigen::MatrixXf V;
+    Eigen::MatrixXf V =
+        ((Eigen::MatrixXf::Random(5, 8) + Eigen::MatrixXf::Ones(5, 8)) *
+         122.5f);
+
     Eigen::MatrixXf H;
     Eigen::MatrixXf W;
 
-    // V = Eigen::MatrixXf(5, 8);
-    // W = Eigen::MatrixXf(5, 2);
-    // H = Eigen::MatrixXf(2, 8);
-
-    // V.setRandom();
-    // V = V.array().abs() * 255;
-    V = ((Eigen::MatrixXf::Random(5, 8) + Eigen::MatrixXf::Ones(5, 8)) *
-         122.5f);
-
-    // Eigen::MatrixXi X = V.cast<int>();
-    // std::cout << X << std::endl;
     int max_iteration = 100;
-    int r = 2;
-    non_negative_matrix_factorization(V, W, H, max_iteration, r);
-    std::cout << H << std::endl;
-    std::cout << W << std::endl;
+    int num_basis = 5;
+    non_negative_matrix_factorization(V, W, H, max_iteration, num_basis);
     return 0;
 }
 
-void non_negative_matrix_factorization(Eigen::MatrixXf V, Eigen::MatrixXf W,
-                                       Eigen::MatrixXf H,
-                                       const int max_iteration, const int r) {
-    int m = V.rows();
-    int n = V.cols();
+void non_negative_matrix_factorization(const Eigen::MatrixXf& V,
+                                       Eigen::MatrixXf& W, Eigen::MatrixXf& H,
+                                       int max_iteration, int num_basis) {
+    int dim_feature = V.rows();
+    int num_feature = V.cols();
 
     // Create H matrix with randomized initial values
-    W = Eigen::MatrixXf(m, r);
+    W = Eigen::MatrixXf(dim_feature, num_basis);
     W.setRandom();
     W.array().abs();
-    W = ((Eigen::MatrixXf::Random(m, r) + Eigen::MatrixXf::Ones(m, r)) *
+    W = ((Eigen::MatrixXf::Random(dim_feature, num_basis) +
+          Eigen::MatrixXf::Ones(dim_feature, num_basis)) *
          122.5f);
     // Create our W with randomiyed initial values
-    H = Eigen::MatrixXf(r, n);
+    H = Eigen::MatrixXf(num_basis, num_feature);
     H.setRandom();
     H.array().abs();
-    H = ((Eigen::MatrixXf::Random(r, n) + Eigen::MatrixXf::Ones(r, n)) *
+    H = ((Eigen::MatrixXf::Random(num_basis, num_feature) +
+          Eigen::MatrixXf::Ones(num_basis, num_feature)) *
          122.5f);
     std::cout << W * H << std::endl;
-    // plot H and W
-    // Create an identity matirx
-    // Eigen::MatrixXf ones = Eigen::MatrixXf::Constant(n, m, 1.0);
+
     float epsilon = 0.0001f;
 
     // Run the multiplicate update rules for max_iteration iterations
@@ -73,11 +63,7 @@ void non_negative_matrix_factorization(Eigen::MatrixXf V, Eigen::MatrixXf W,
         W = W.array() * step_length_W.array();
         // std::cout << W    << std::endl;
         float loss = (V - W * H).array().square().sum();
-        // std::cout << "norm(V-WH) is : " << loss << '\n';
+        std::cout << "norm(V-WH) is : " << loss << '\n';
     }
-    std::cout << V << std::endl;
-    std::cout << "************" << std::endl;
-    std::cout << W * H << std::endl;
-    std::cout << "************" << std::endl;
-    std::cout << V - W * H << std::endl;
+    std::cout << "V - W*H = \n" << V - W * H << std::endl;
 }
