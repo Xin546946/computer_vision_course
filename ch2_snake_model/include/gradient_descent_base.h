53b7c2147c06f686ce6d1e08876512bf085b831a
zhaoran <zhaoran.wu1@gmail.com>
2020-11-05 12:42:04 +0100

    change base

--- ch2_snake_model/include/gradient_descent_base.h
+++ ch2_snake_model/include/gradient_descent_base.h
@@ -1,12 +1,22 @@
 #pragma once
+#include <numeric>
 
 class GradientDescentBase {
    public:
+    GradientDescentBase() = default;
     void run(int max_iteration);
 
    private:
     virtual void initialize() = 0;
     virtual void update() = 0;
     virtual bool is_terminate(int current_iter, int max_iteration) const;
+
+    virtual float computer_energy() = 0;
+    virtual void roll_back_state() = 0;
+    virtual void update_step_size(bool is_energy_decent);
+
     virtual void print_terminate_info() const;
+
+    float energy_ = std::numeric_limits<float>::max();
+    float step_size_ = 1e-10;
 };
--- ch2_snake_model/src/gradient_descent_base.cpp
+++ ch2_snake_model/src/gradient_descent_base.cpp
@@ -4,10 +4,20 @@
 
 void GradientDescentBase::run(int max_iteration) {
     initialize();
+    float last_energy = computer_energy();
     int current_iter = 0;
     while (!is_terminate(current_iter, max_iteration)) {
         current_iter++;
         update();
+
+        energy_ = computer_energy();
+        if (energy_ < last_energy) {
+            update_step_size(true);
+            last_energy = energy_;
+        } else {
+            update_step_size(false);
+            roll_back_state();
+        }
     }
     print_terminate_info();
 }
@@ -18,4 +28,8 @@ bool GradientDescentBase::is_terminate(int current_iter,
 }
 void GradientDescentBase::print_terminate_info() const {
     std::cout << "Iteration finished" << std::endl;
+}
+
+void GradientDescentBase::update_step_size(bool is_energy_decent) {
+    step_size_ *= (is_energy_decent) ? 1.5f : 0.5f;
 }
