d17d2aa91019e4a2387322db9e983b7ffd1b7eb3
Xin Jin <xinjin1109@gmail.com>
2020-11-13 22:42:20 +0100

    test bug

--- ch3_level_set/src/sdf_map.cpp
+++ ch3_level_set/src/sdf_map.cpp
@@ -1,1 +1,11 @@
 #include "sdf_map.h"
+
+SDFMap::SDFMap(int rows, int cols, cv::Point2d center, double radius)
+    : map_(cv::Mat::zeros(cv::Size(cols, rows), CV_64F)) {
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            map_.at<double>(r, c) =
+                std::sqrt(pow(r - cols / 2, 2) + pow(c - rows / 2, 2));
+        }
+    }
+}
--- ch3_level_set/src/test_sdf.cpp
+++ ch3_level_set/src/test_sdf.cpp
@@ -1,4 +1,4 @@
-#include "display.cpp"
+#include "display.h"
 #include <opencv2/core.hpp>
 int main(char argc, char** argv) {
     // define and and initialize a sdf_map object
@@ -8,6 +8,7 @@ int main(char argc, char** argv) {
     cv::Point2d center(rows / 2.f, cols / 2.f);
     double radius = std::min(rows, cols) / 4.f;
     SDFMap sdf_map(rows, cols, center, radius);
-    cv::Mat apply_jetmap(sdf_map);
+
+    cv::Mat draw_sdf_map = draw_sdf_map(sdf_map);
     return 0;
 }
