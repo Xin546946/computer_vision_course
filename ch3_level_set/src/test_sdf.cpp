b808a3347ff3bbb7cae6e09effffe137aa5c82a6
Xin Jin <xinjin1109@gmail.com>
2020-11-13 22:24:57 +0100

    test function for ctor

--- ch3_level_set/src/test_sdf.cpp
+++ ch3_level_set/src/test_sdf.cpp
@@ -1,5 +1,13 @@
 #include "display.cpp"
-
-int main() {
+#include <opencv2/core.hpp>
+int main(char argc, char** argv) {
+    // define and and initialize a sdf_map object
+    cv::Mat img = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+    int rows = img.rows;
+    int cols = img.cols;
+    cv::Point2d center(rows / 2.f, cols / 2.f);
+    double radius = std::min(rows, cols) / 4.f;
+    SDFMap sdf_map(rows, cols, center, radius);
+    cv::Mat apply_jetmap(sdf_map);
     return 0;
 }
