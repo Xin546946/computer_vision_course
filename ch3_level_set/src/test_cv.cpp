75c619af7fb9a24f39890e965b8c6f4d3ed003ce
Xin Jin <xinjin1109@gmail.com>
2020-11-14 22:29:58 +0100

    gaussian kernel done

--- ch3_level_set/src/test_cv.cpp
+++ ch3_level_set/src/test_cv.cpp
@@ -2,19 +2,26 @@
 #include "level_set_utils.h"
 #include "sdf_map.h"
 #include <opencv2/core.hpp>
+#include <opencv2/core/types.hpp>
 
 int main(int argc, char** argv) {
     // define and and initialize a sdf_map object
-    cv::Mat img = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
-    int size = 399;
-    cv::Rect mask(0, 0, size, size);
-    img = img(mask);
+    cv::Mat image = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+    disp_image(image, "face", 0);
+    std::cout << image.size() << std::endl;
+    image.convertTo(image, CV_64F);
+    int size = 517;
+    cv::Rect mask(180, 0, size, size);
+
+    cv::Mat img = image(mask);
+    std::cout << "w " << std::endl;
     assert(img.rows == img.cols && img.rows * img.cols % 2 == 1);
 
-    double sigma = 1;
+    double sigma = 100;
     cv::Mat gauss_kernel = gaussian_kernel(size, sigma);
     cv::Mat gauss_result = gauss_kernel.mul(img);
-
-    disp_image(gauss_result, "gaussian_kernel", 0);
+    cv::normalize(gauss_kernel, gauss_kernel, 0, 255, cv::NORM_MINMAX);
+    gauss_kernel.convertTo(gauss_kernel, CV_8UC1);
+    disp_image(gauss_kernel, "gaussian_kernel", 0);
     return 0;
 }
