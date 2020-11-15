cd5e629020da29704b4df8daca252c80a0298af7
Xin Jin <xinjin1109@gmail.com>
2020-11-15 18:00:51 +0100

    change spell  mistake

--- ch3_level_set/src/level_set_utils.cpp
+++ ch3_level_set/src/level_set_utils.cpp
@@ -89,8 +89,8 @@ cv::Mat compute_derivative_data_term(const SDFMap& sdf_map,
 
     cv::Mat vis;
     cv::Mat vis_dirac = get_float_mat_vis_img(dirac(sdf_map, eps));
-    cv::Mat vis_e_foreground = get_float_mat_vis_img(e_foregroud);
-    cv::Mat vis_e_background = get_float_mat_vis_img(e_backgroud);
+    cv::Mat vis_e_foreground = get_float_mat_vis_img(e_foreground);
+    cv::Mat vis_e_background = get_float_mat_vis_img(e_background);
 
     cv::hconcat(vis_dirac, vis_e_foreground, vis);
     cv::hconcat(vis, vis_e_background, vis);
