1b3ba623d7bb3854deb8872533cd00320e007325
zhaoran <zhaoran.wu1@gmail.com>
2020-11-16 20:28:56 +0100

    open visualization

--- ch3_level_set/src/test_cv.cpp
+++ ch3_level_set/src/test_cv.cpp
@@ -8,18 +8,12 @@ int main(int argc, char** argv) {
     // define and and initialize a sdf_map object
     cv::Mat img = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
     cv::GaussianBlur(img, img, cv::Size(3, 3), 3);
-    int rows = img.rows;
-    int cols = img.cols;
-    cv::Point2d center(cols / 2.f, rows / 2.f);
-    double radius = std::min(rows, cols) / 2.1f;
     ParamLevelSetCV param_level_set_cv(
         1e-2, 1e-2, 1.0, 1e-2, 20,
         1.2);  // fore_weight, back_weight, eps,
                // step_size,l_w,g_w; 1.1,1.0,5e-2,15,1,2
 
     cv::Mat dx = do_sobel(img, 1);
-    cv::Mat vis_dx = get_float_mat_vis_img(dx);
-    disp_image(vis_dx, "dx");
 
     LevelSetCV level_set_cv(img, param_level_set_cv);
     level_set_cv.run(1e4);
