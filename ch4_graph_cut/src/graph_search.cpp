987fa1b9b5c73bc733163a1cd706ed6f6fa33286
zhaoran <zhaoran.wu1@gmail.com>
2020-11-21 17:39:31 +0100

    dfs

--- ch4_graph_cut/src/graph_search.cpp
+++ ch4_graph_cut/src/graph_search.cpp
@@ -1,4 +1,15 @@
 #include "graph_search.h"
+#include <iostream>
 
 void DFS(Node* root, std::vector<bool>& visited) {
+    visited[root->id_] = true;
+
+    if (!root) return;
+
+    for (auto& elem : root->children_) {  // (*root).children_
+        if (!elem.first->id_) {
+            std::cout << elem.first->id_ << '\n';
+            DFS(elem.first, visited);
+        }
+    }
 }
