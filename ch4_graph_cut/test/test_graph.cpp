3d9a2cd4d83cf05db03dfaf12282f9e1dc9f2df0
zhaoran <zhaoran.wu1@gmail.com>
2020-11-22 16:44:50 +0100

    change test

--- ch4_graph_cut/test/test_graph.cpp
+++ ch4_graph_cut/test/test_graph.cpp
@@ -3,12 +3,12 @@
 
 int main(int argc, char** argv) {
     int num_node;
-    Graph graph(num_node);
+    Graph<Node> graph(num_node);
 
-    graph.add_binary_node(2, 3, 0.5);
-    graph.add_binary_node(2, 1e5, 0.5);
-    graph.add_unary_node(0, 5, 0.8);
-    graph.add_unary_node(0, 7, 0.8);
+    graph.add_binary_edge(2, 3, 0.5);
+    graph.add_binary_edge(2, 1e5, 0.5);
+    graph.add_unary_edge(0, 5, 0.8);
+    graph.add_unary_edge(0, 7, 0.8);
 
     DFS(graph.get_root());
 
