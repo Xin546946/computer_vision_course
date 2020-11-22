c265fdbd3d64033935e38782f5636f017fa23971
zhaoran <zhaoran.wu1@gmail.com>
2020-11-22 16:03:11 +0100

    graph test

--- ch4_graph_cut/test/test_graph.cpp
+++ ch4_graph_cut/test/test_graph.cpp
@@ -2,5 +2,15 @@
 #include <iostream>
 
 int main(int argc, char** argv) {
+    int num_node;
+    Graph graph(num_node);
+
+    graph.add_binary_node(2, 3, 0.5);
+    graph.add_binary_node(2, 1e5, 0.5);
+    graph.add_unary_node(0, 5, 0.8);
+    graph.add_unary_node(0, 7, 0.8);
+
+    DFS(graph.get_root());
+
     return 0;
 }
