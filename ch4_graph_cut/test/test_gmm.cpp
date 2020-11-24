dcadc3acd0ae4ae2c529a3776b755b0b58c7810b
Xin Jin <xinjin1109@gmail.com>
2020-11-24 19:46:30 +0100

    test gmm

--- ch4_graph_cut/test/test_gmm.cpp
+++ ch4_graph_cut/test/test_gmm.cpp
@@ -8,18 +8,18 @@ int main(int argc, char** argv) {
     cv::Mat img = cv::imread(argv[1], cv::IMREAD_COLOR);
 
     GMM gmm(img, 2);  // input: image, num_model
-    gmm.run(20);
+    gmm.run(1);
 
     // // test each point w.r.t. the GMM
     cv::Mat prob1 = gmm.get_sub_prob(img, 0);
-    cv::Mat prob_vis_1 = get_float_mat_vis_img(prob1, 3);
+    cv::Mat prob_vis_1 = get_float_mat_vis_img(prob1);
     // cv::cvtColor(prob_vis_1, prob_vis_1, CV_GRAY2RGB);
     cv::Mat prob2 = gmm.get_sub_prob(img, 1);
-    cv::Mat prob_vis_2 = get_float_mat_vis_img(prob2, 3);
+    cv::Mat prob_vis_2 = get_float_mat_vis_img(prob2);
     // cv::cvtColor(prob_vis_2, prob_vis_2, CV_GRAY2RGB);
     cv::Mat vis;
-    cv::vconcat(img, prob_vis_1, vis);
-    cv::vconcat(vis, prob_vis_2, vis);
+    // cv::vconcat(img, prob_vis_1, vis);
+    cv::vconcat(prob_vis_1, prob_vis_2, vis);
     disp_image(vis, "clustering resut", 0);
 
     // // test each point w.r.t. the first model
