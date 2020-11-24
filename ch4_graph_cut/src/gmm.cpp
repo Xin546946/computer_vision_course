86f677892db6ef8806c16edce7439aa98ee0da7e
zhaoran <zhaoran.wu1@gmail.com>
2020-11-24 17:20:56 +0100

    debug to posterior

--- ch4_graph_cut/src/gmm.cpp
+++ ch4_graph_cut/src/gmm.cpp
@@ -1,4 +1,5 @@
 #include "gmm.h"
+#include <iostream>
 #include <opencv2/core/core.hpp>
 #include <random>
 #include <set>
@@ -18,19 +19,19 @@ Gaussian3D::Gaussian3D(const cv::Matx31d& miu, const cv::Matx33d& sigma)
 
 double Gaussian3D::compute_gaussian_data(const cv::Matx31d& data) {
     double coeff = cv::pow(M_1_PI * 0.5, 3 / 2) / cv::determinant(sigma_);
-    cv::Mat tmp;
-    cv::exp(-0.5 * (data - miu_).t() * sigma_.inv() * (data - miu_), tmp);
-    return coeff * tmp.at<double>(0, 0);
+    auto tmp = -0.5 * (data - miu_).t() * sigma_.inv() * (data - miu_);
+    return coeff * exp(tmp(0));
 }
 
 cv::Mat Gaussian3D::compute_gaussian_map(cv::Mat img) {
     cv::Mat result = cv::Mat::zeros(cv::Size(img.cols, img.rows), CV_64F);
     for (int r = 0; r < img.rows; r++) {
-        for (int c = 0; r < img.cols; c++) {
+        for (int c = 0; c < img.cols; c++) {
             result.at<double>(r, c) =
                 compute_gaussian_data(img.at<cv::Vec3b>(r, c));
         }
     }
+
     return result;
 }
 cv::Matx31d Gaussian3D::get_miu() const {
@@ -45,6 +46,7 @@ void Gaussian3D::set_miu(const cv::Matx31d& miu) {
     miu_ = miu;
 }
 
+// todo change name --> sigma square
 void Gaussian3D::set_sigma(const cv::Matx33d& sigma) {
     sigma_ = sigma;
 }
@@ -56,7 +58,7 @@ GMM::GMM(cv::Mat img, int num_gaussian)
       img_(img),
       w_gaussian_model_(num_gaussian, 1.0 / num_gaussian),
       gaussian3d_model_(num_gaussian),
-      posterior_(num_gaussian, cv::Mat::zeros(cv::Size(3, 3), img.type())) {
+      posterior_(num_gaussian, cv::Mat::zeros(img.size(), CV_64FC1)) {
 }
 
 std::set<int> get_random_index(int max_idx, int n) {
@@ -74,16 +76,16 @@ void GMM::initialize() {
     std::set<int> random_idx =
         get_random_index(img_.rows * img_.cols - 1, gaussian3d_model_.size());
     for (auto it = random_idx.begin(); it != random_idx.end(); it++) {
-        cv::Matx31d miu = img_.at<cv::Vec3b>(*it % img_.cols, *it / img_.cols);
-        cv::Matx33d sigma = cv::Matx33d::eye();
+        cv::Matx31d miu = img_.at<cv::Vec3b>(*it / img_.cols, *it % img_.cols);
+        cv::Matx33d sigma_square = 150 * 150 * cv::Matx33d::eye();
         gaussian3d_model_[std::distance(random_idx.begin(), it)].set_miu(miu);
         gaussian3d_model_[std::distance(random_idx.begin(), it)].set_sigma(
-            sigma);
+            sigma_square);
     }
 }
 
 void GMM::update_e_step() {
-    cv::Mat sum = cv::Mat::zeros(img_.size(), img_.type());
+    cv::Mat sum = cv::Mat::zeros(posterior_[0].size(), posterior_[0].type());
     for (int i = 0; i < gaussian3d_model_.size(); i++) {
         posterior_[i] = gaussian3d_model_[i].compute_gaussian_map(img_);
         sum += w_gaussian_model_[i] * posterior_[i];
@@ -91,6 +93,7 @@ void GMM::update_e_step() {
 
     for (int i = 0; i < gaussian3d_model_.size(); i++) {
         cv::divide(posterior_[i], sum, posterior_[i]);
+        std::cerr << "sum :" << cv::sum(posterior_[i])[0];
     }
 }
 
@@ -104,9 +107,14 @@ void GMM::update_m_step() {
 }
 
 void GMM::update_miu(int id_model, double Nk) {
-    cv::Matx31d new_miu =
-        (1 / (Nk + 1e-10)) *
-        gaussian3d_model_[id_model].compute_gaussian_map(img_).dot(img_);
+    cv::Matx31d new_miu = cv::Matx31d::zeros();
+    for (int r = 0; r < img_.rows; r++) {
+        for (int c = 0; c < img_.cols; c++) {
+            new_miu += posterior_[id_model].at<double>(r, c) *
+                       img_.at<cv::Matx31d>(r, c);
+        }
+    }
+
     gaussian3d_model_[id_model].set_miu(new_miu);
 }
 
