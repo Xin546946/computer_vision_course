1ba4719731657f9a237ce85034904cc5117d28ff
zhaoran <zhaoran.wu1@gmail.com>
2020-12-04 23:43:12 +0100

    change name space

--- ch5_motion_based_segmentation/src/gmm.cpp
+++ ch5_motion_based_segmentation/src/gmm.cpp
@@ -2,39 +2,41 @@
 #include <algorithm>
 #include <cmath>
 #include <iostream>
+namespace gmm {
+
 /*--------------------------------------------------------
 #####################implementation: ConfigParam #####################
 ---------------------------------------------------------*/
-gmm::ConfigParam::ConfigParam(double a, double alpha, double fore_threshold)
+ConfigParam::ConfigParam(double a, double alpha, double fore_threshold)
     : a_(a), alpha_(alpha), fore_threshold_(fore_threshold) {
 }
 
 /*--------------------------------------------------------
 #####################implementation: GaussianParam #####################
 ---------------------------------------------------------*/
-gmm::GaussianParam::GaussianParam(double mean, double var, double weight) : mean_(mean), var_(var), weight_(weight) {
+GaussianParam::GaussianParam(double mean, double var, double weight) : mean_(mean), var_(var), weight_(weight) {
 }
 
 /*--------------------------------------------------------
 #####################implementation: compute_gaussian_pdf #####################
 ---------------------------------------------------------*/
 
-double gmm::compute_gaussian_pdf(gmm::GaussianParam param, double sample) {
+double compute_gaussian_pdf(gmm::GaussianParam param, double sample) {
     return (1 / (sqrt(2 * M_PI) * param.var_)) * exp(-0.5 * pow((sample - param.mean_) / param.var_, 2));
 }
 
 /*--------------------------------------------------------
 #####################implementation: ModelParam #####################
 ---------------------------------------------------------*/
-bool gmm::operator<(const gmm::GaussianParam& lhs, const gmm::GaussianParam& rhs) {
+bool operator<(const GaussianParam& lhs, const GaussianParam& rhs) {
     return (lhs.weight_ / lhs.var_) < (rhs.weight_ / rhs.var_);
 }
 
-void gmm::ModelParam::sort() {
+void ModelParam::sort() {
     std::sort(this->param_.begin(), this->param_.end());
 }
 
-std::ostream& gmm::operator<<(std::ostream& os, const gmm::ModelParam& model_param) {
+std::ostream& operator<<(std::ostream& os, const ModelParam& model_param) {
     for (int i = 0; i < model_param.param_.size(); i++) {
         const gmm::GaussianParam& param = model_param.param_[i];
         std::cout << "model id : " << i << " mean : " << param.mean_ << " var : " << param.var_
@@ -45,14 +47,13 @@ std::ostream& gmm::operator<<(std::ostream& os, const gmm::ModelParam& model_par
 /*--------------------------------------------------------
 #####################implementation: GMM #####################
 ---------------------------------------------------------*/
-gmm::GMM::GMM(int num_gaussians, ConfigParam config_param)
-    : num_gaussians_(num_gaussians), config_param_(config_param) {
+GMM::GMM(int num_gaussians, ConfigParam config_param) : num_gaussians_(num_gaussians), config_param_(config_param) {
 }
 
-gmm::ModelParam gmm::GMM::get_model_param() const {
+ModelParam GMM::get_model_param() const {
     return this->model_param_;
 }
-void gmm::GMM::add_sample(double sample) {
+void GMM::add_sample(double sample) {
     if (!is_in_gmm(sample)) {
         replace_model(sample);
     } else {
@@ -60,18 +61,18 @@ void gmm::GMM::add_sample(double sample) {
     }
 }
 
-bool gmm::GMM::is_in_gmm(double sample) {
+bool GMM::is_in_gmm(double sample) {
     model_param_.sort();
     return abs(sample - model_param_.param_[0].mean_) < config_param_.a_ * model_param_.param_[0].var_;
 }
 
-void gmm::GMM::replace_model(double sample) {
+void GMM::replace_model(double sample) {
     model_param_.sort();
     model_param_.param_[0].mean_ = sample;
     model_param_.param_[0].var_ = 50;
 }
 
-void gmm::GMM::update_gmm(double sample) {
+void GMM::update_gmm(double sample) {
     for (int i = 0; i < num_gaussians_; i++) {
         double sample_pdf = gmm::compute_gaussian_pdf(model_param_.param_[i], sample);
         model_param_.param_[i].weight_ =
@@ -82,3 +83,5 @@ void gmm::GMM::update_gmm(double sample) {
                                       ro * pow(sample - model_param_.param_[i].mean_, 2);
     }
 }
+
+}  // namespace gmm
