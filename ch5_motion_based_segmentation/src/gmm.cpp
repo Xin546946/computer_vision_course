a5dae14ca2ba0dd41ba5a0ac6bb005fdfc46f931
Xin Jin <xinjin1109@gmail.com>
2020-12-04 22:02:15 +0100

    add changes

--- ch5_motion_based_segmentation/src/gmm.cpp
+++ ch5_motion_based_segmentation/src/gmm.cpp
@@ -1,7 +1,5 @@
 #include "gmm.h"
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-
+#include <cmath>
 /*--------------------------------------------------------
 #####################implementation: ConfigParam #####################
 ---------------------------------------------------------*/
@@ -12,21 +10,56 @@ gmm::ConfigParam::ConfigParam(double a, double alpha, double fore_threshold)
 /*--------------------------------------------------------
 #####################implementation: GaussianParam #####################
 ---------------------------------------------------------*/
+gmm::GaussianParam::GaussianParam(double mean, double var, double weight)
+    : mean_(mean), var_(var), weight_(weight) {
+}
+
+/*--------------------------------------------------------
+#####################implementation: ModelParam #####################
+---------------------------------------------------------*/
+
+int gmm::ModelParam::get_size() const {
+    return param_.size();
+}
 
 /*--------------------------------------------------------
 #####################implementation: GMM #####################
 ---------------------------------------------------------*/
-gmm::GMM::GMM(int num_gaussian, ConfigParam config_param)
-    : num_gaussian_(num_gaussian), config_param_(config_param), model_param_() {
+gmm::GMM::GMM(int num_gaussians, ConfigParam config_param)
+    : num_gaussians_(num_gaussians),
+      config_param_(config_param),
+      model_param_() {
 }
 
 void gmm::GMM::add_sample(double sample) {
     if (!is_in_gmm(sample)) {
-        replace_model();
+        replace_model(sample);
     } else {
         update_gmm();
     }
 }
 
 bool gmm::GMM::is_in_gmm(double sample) {
+    return abs(sample - model_param_.param_.top().mean_) <
+           config_param_.a_ * model_param_.param_.top().var_;
+}
+
+void gmm::GMM::replace_model(double sample) {
+    double weight = model_param_.param_.top().weight_;
+    model_param_.param_.pop();
+    model_param_.param_.emplace(sample, 50, weight);
+}
+
+void gmm::GMM::update_gmm() {
+    update_weight();
+    double ro = compute_ro();
+    update_mean();
+    update_var();
+}
+
+void gmm::GMM::update_weight(double sample) {
+    for (auto it = model_param_.param_.begin(); it != model_param_.param_.end();
+         it++) {
+        *it.weight
+    }
 }
--- ch5_motion_based_segmentation/src/gmm.h
+++ ch5_motion_based_segmentation/src/gmm.h
@@ -1,14 +1,7 @@
 #pragma once
 
-#include <opencv2/core/core.hpp>
 #include <queue>
 #include <vector>
-
-struct GaussianParam {
-    double mean_;
-    double var_;
-    double weight_;
-};
 
 namespace gmm {
 
@@ -19,11 +12,25 @@ struct ConfigParam {
     double fore_threshold_;
 };
 
+struct GaussianParam {
+    GaussianParam(double mean, double var, double weight);
+    double mean_ = 0;
+    double var_ = 50;
+    double weight_ = 1;
+};
+
+struct Gaussian {
+    Gaussian(GaussianParam param);
+    void compute_gaussian_pdf() const;
+};
+
 bool operator<(const GaussianParam& lhs, const GaussianParam& rhs) {
-    return (lhs.weight_ / lhs.var_) < (rhs.weight_ / rhs.var_);
+    return (lhs.weight_ / lhs.var_) > (rhs.weight_ / rhs.var_);
 }
 struct ModelParam {
-    std::priority_queue<GaussianParam> model_param;
+    std::priority_queue<GaussianParam> param_;
+
+    int get_size() const;
 };
 
 class GMM {
@@ -35,10 +42,13 @@ class GMM {
 
    private:
     bool is_in_gmm(double sample);
-    void replace_model();
+    void replace_model(double sample);
     void update_gmm();
-
-    int num_gaussian_;
+    void update_mean();
+    void update_var();
+    void update_weight(double sample);
+    double compute_ro();
+    int num_gaussians_;
     ConfigParam config_param_;
     ModelParam model_param_;
 };
