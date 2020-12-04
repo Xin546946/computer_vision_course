19be258744740223697bb2a08d8aa0e48a566b87
Xin Jin <xinjin1109@gmail.com>
2020-12-04 20:33:42 +0100

    add GMM class undone

--- ch5_motion_based_segmentation/src/gmm.cpp
+++ ch5_motion_based_segmentation/src/gmm.cpp
@@ -1,1 +1,32 @@
 #include "gmm.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+/*--------------------------------------------------------
+#####################implementation: ConfigParam #####################
+---------------------------------------------------------*/
+gmm::ConfigParam::ConfigParam(double a, double alpha, double fore_threshold)
+    : a_(a), alpha_(alpha), fore_threshold_(fore_threshold) {
+}
+
+/*--------------------------------------------------------
+#####################implementation: GaussianParam #####################
+---------------------------------------------------------*/
+
+/*--------------------------------------------------------
+#####################implementation: GMM #####################
+---------------------------------------------------------*/
+gmm::GMM::GMM(int num_gaussian, ConfigParam config_param)
+    : num_gaussian_(num_gaussian), config_param_(config_param), model_param_() {
+}
+
+void gmm::GMM::add_sample(double sample) {
+    if (!is_in_gmm(sample)) {
+        replace_model();
+    } else {
+        update_gmm();
+    }
+}
+
+bool gmm::GMM::is_in_gmm(double sample) {
+}
---
+++ ch5_motion_based_segmentation/src/gmm.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <opencv2/core/core.hpp>
+#include <queue>
+#include <vector>
+
+struct GaussianParam {
+    double mean_;
+    cv::Matx33d var_;
+};
+
+namespace gmm {
+
+struct ConfigParam {
+    ConfigParam(double a, double alpha, double fore_threshold);
+    double a_;
+    double alpha_;
+    double fore_threshold_;
+};
+
+struct ModelParam {
+    std::priority_queue<GaussianParam> model_param;
+    std::vector<double> weight_;
+};
+
+class GMM {
+   public:
+    GMM(int num_gaussian, ConfigParam config_param);
+    void add_sample(double sample);
+    ModelParam get_model_param() const;
+    bool is_foreground() const;
+
+   private:
+    bool is_in_gmm(double sample);
+    void replace_model();
+    void update_gmm();
+
+    int num_gaussian_;
+    ConfigParam config_param_;
+    ModelParam model_param_;
+};
+}  // namespace gmm
