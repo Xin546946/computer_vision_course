47a3d53cc853bb1cf1a97d27274ac4d72066456c
zhaoran <zhaoran.wu1@gmail.com>
2020-12-11 21:53:52 +0100

    add bbox and test

---
+++ ch6_optical_flow/src/bounding_box.h
@@ -0,0 +1,28 @@
+#include <opencv2/core/core.hpp>
+
+typedef cv::Point Point;
+
+class BoundingBox {
+   public:
+    BoundingBox(int x, int y, int width, int height) : window_(x, y, width, height){};
+
+    void move(float delta_x, float delta_y) {
+        window_.x += delta_x;
+        window_.y += delta_y;
+    }
+
+    const Point top_left() const {
+        return window_.tl();
+    }
+
+    const int width() const {
+        return window_.width;
+    }
+
+    const int height() const {
+        return window_.height;
+    }
+
+   private:
+    cv::Rect2i window_;
+};
--- ch6_optical_flow/test/test_optical_flow_tracker.cpp
+++ ch6_optical_flow/test/test_optical_flow_tracker.cpp
@@ -1,9 +1,25 @@
+#include "bounding_box.h"
+#include "opencv_utils.h"
+#include "optical_flow_tracker.h"
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
 int main(int argc, char** argv) {
-    cv::Mat img = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+    std::vector<cv::Mat> video;
+    for (int id = 1; id < 914; id++) {
+        cv::Mat img = cv::imread(argv[1] + std::to_string(id) + ".jpg", cv::IMREAD_GRAYSCALE);
+        video.push_back(img);
+    }
+
+    // detect object in the firse image with template
+    cv::Mat temp = cv::imread(argv[2], cv::IMREAD_GRAYSCALE);
+    cv::Point2i center = template_matching(video[0], temp);
+
+    BoundingBox bbox_init(center.x, center.y, template.cols, template.row);
+
+    OpticalFlowTracker tracker(win_init);
+    tracker.process(video);
 
     return 0;
 }
