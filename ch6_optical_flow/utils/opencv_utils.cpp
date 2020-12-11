958ec883cc4ce830eec6cb6c6e0f4f1727b9d5eb
zhaoran <zhaoran.wu1@gmail.com>
2020-12-11 22:11:14 +0100

    add template mathcing function

--- ch6_optical_flow/utils/opencv_utils.cpp
+++ ch6_optical_flow/utils/opencv_utils.cpp
@@ -1,1 +1,33 @@
 #include "opencv_utils.h"
+#include "opencv2/imgproc.hpp"
+
+std::vector<cv::Mat> record_webcam() {
+    std::vector<cv::Mat> result;
+    cv::VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if (!cap.open(0)) return {};
+    while (true) {
+        cv::Mat frame;
+        cap >> frame;
+        if (frame.empty()) break;  // end of video stream
+        cv::imshow("this is you, smile! :)", frame);
+        auto key = cv::waitKey(10);
+        if (key == 27) {
+            break;  // stop capturing by pressing ESC
+        } else {
+            result.push_back(frame);
+        }
+    }
+    return result;
+}
+
+cv::Point2i template_matching(cv::Mat img, cv::Mat temp) {
+    assert(!img.empty() && !temp.empty());
+    cv::Mat result;
+    cv::matchTemplate(img, temp, result, cv::TM_CCORR_NORMED);
+    double val_min, val_max;
+    cv::Point center_min, center_max;
+    cv::minMaxLoc(result, &val_min, &val_max, &center_min, &center_max, cv::Mat());
+    return center_max;
+}
--- ch6_optical_flow/utils/opencv_utils.h
+++ ch6_optical_flow/utils/opencv_utils.h
@@ -13,23 +13,6 @@ inline cv::Point id_to_pos(int id, int step) {
     return {id / step, id % step};
 }
 
-std::vector<cv::Mat> record_webcam() {
-    std::vector<cv::Mat> result;
-    cv::VideoCapture cap;
-    // open the default camera, use something different from 0 otherwise;
-    // Check VideoCapture documentation.
-    if (!cap.open(0)) return {};
-    while (true) {
-        cv::Mat frame;
-        cap >> frame;
-        if (frame.empty()) break;  // end of video stream
-        cv::imshow("this is you, smile! :)", frame);
-        auto key = cv::waitKey(10);
-        if (key == 27) {
-            break;  // stop capturing by pressing ESC
-        } else {
-            result.push_back(frame);
-        }
-    }
-    return result;
-}
+std::vector<cv::Mat> record_webcam();
+
+cv::Point2i template_matching(cv::Mat img, cv::Mat temp);
