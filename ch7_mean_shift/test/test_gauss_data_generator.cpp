77be8876b261c7c539c2375da3342786264c9e72
zhaoran <zhaoran.wu1@gmail.com>
2020-12-19 22:38:17 +0100

    test mean shift

--- ch7_mean_shift/test/test_gauss_data_generator.cpp
+++ ch7_mean_shift/test/test_gauss_data_generator.cpp
@@ -5,24 +5,51 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+void draw_circles(const std::vector<std::array<double, 2>>& data, cv::Mat img, cv::Scalar bgr) {
+    for (auto d : data) {
+        cv::Point2d p(d[0], d[1]);
+        cv::circle(img, p, 1, bgr, 2);
+    }
+}
+
 int main(int argc, char** argv) {
     auto data1 = generate_gauss_data<double, 2>(1000, {500, 500}, {100, 50});
     auto data2 = generate_gauss_data<double, 2>(1000, {80, 800}, {30, 30});
 
+    auto all_data = data1;
+    std::copy(data2.begin(), data2.end(), std::back_inserter(all_data));
+
     cv::Mat img = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
 
-    for (auto d : data1) {
-        cv::Point2d p(d[0], d[1]);
-        cv::circle(img, p, 1, cv::Scalar(0, 255, 0), 2);
-    }
+    draw_circles(data1, img, cv::Scalar(0, 255, 0));
+    draw_circles(data2, img, cv::Scalar(255, 0, 0));
+    // cv::imshow("img", img);
+    // cv::waitKey(0);
 
-    for (auto d : data2) {
-        cv::Point2d p(d[0], d[1]);
-        cv::circle(img, p, 1, cv::Scalar(255, 0, 0), 2);
-    }
+    double r_square = 100 * 100;
 
-    cv::imshow("img", img);
-    cv::waitKey(0);
+    while (true) {
+        cv::Mat vis = cv::Mat::zeros(cv::Size(1000, 1000), CV_8UC3);
+        draw_circles(all_data, vis, cv::Scalar(0, 255, 0));
+        cv::imshow("img", vis);
+        cv::waitKey(1);
+        for (int i = 0; i < all_data.size(); i++) {
+            double acc_x = 0.0;
+            double acc_y = 0.0;
+            int num = 0;
+            for (int j = 0; j < all_data.size(); j++) {
+                auto d1 = all_data[i];
+                auto d2 = all_data[j];
+
+                if (std::pow(d1[0] - d2[0], 2.0) + std::pow(d1[1] - d2[1], 2.0) > r_square) continue;
+
+                acc_x += d2[0];
+                acc_y += d2[1];
+                num++;
+            }
+            all_data[i] = {acc_x / num, acc_y / num};
+        }
+    }
 
     return 0;
 }
