0b192772225d8c5fc5ad80b18b680f8994b98170
zhaoran <zhaoran.wu1@gmail.com>
2020-12-19 23:41:12 +0100

    add color data

---
+++ ch7_mean_shift/src/data_base.cpp
@@ -0,0 +1,11 @@
+#include "data_base.h"
+#include "memory"
+#include "window_base.h"
+
+void ColorData::update_mass_center(const std::unique_ptr<WindowBase>& win_ptr) {
+}
+
+void ColorData::visualize() const {
+}
+bool ColorData::is_convergent() {
+}
--- ch7_mean_shift/src/data_base.h
+++ ch7_mean_shift/src/data_base.h
@@ -1,11 +1,23 @@
 #pragma once
+#include <opencv2/core/core.hpp>
 
 class DataBase {
    public:
     virtual void init_mass_center() = 0;
     virtual void update_mass_center(const std::unique_ptr<WindowBase>& win_ptr) = 0;
-    virtual void visualize() const;
-    virtual bool is_convergent();
+    virtual void visualize() const = 0;
+    virtual bool is_convergent() = 0;
 
    private:
+};
+
+class ColorData : public DataBase {
+   public:
+    void init_mass_center() override{};
+    void update_mass_center(const std::unique_ptr<WindowBase>& win_ptr) override;
+    void visualize() const override;
+    bool is_convergent() override;
+
+   private:
+    std::vector<cv::Vec3d> colors;
 };
