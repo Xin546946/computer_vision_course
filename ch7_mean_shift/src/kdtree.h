e3c9cf6b417b04823ed64c606c75a8098ab123e5
zhaoran <zhaoran.wu1@gmail.com>
2020-12-23 21:55:51 +0100

    apply fast equal

--- ch7_mean_shift/src/kdtree.h
+++ ch7_mean_shift/src/kdtree.h
@@ -110,9 +110,14 @@ template <typename T, int Dim>
 KDTreeNode<T, Dim>* search_data_recursively(KDTreeNode<T, Dim>* curr, const std::array<T, Dim>& data) {
     if (curr->has_leaves()) {
         for (KDTreeNode<T, Dim>* leaf : curr->children_) {
-            if (leaf->data_ == data) {
-                return leaf;
+            bool equal = true;
+            for (int i = 0; i < Dim; i++) {
+                if (leaf->data_[i] != data[i]) {
+                    equal = false;
+                    break;
+                }
             }
+            if (equal) return leaf;
         }
     } else {
         int axis = curr->axis_;
