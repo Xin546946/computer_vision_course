512ff0322c95aa19f2fbb26c70b8e0fc1159f0d2
zhaoran <zhaoran.wu1@gmail.com>
2020-12-27 00:05:33 +0100

    remove dependecies

--- ch7_mean_shift/test/test_gkdtree_mean_shift.cpp
+++ ch7_mean_shift/test/test_gkdtree_mean_shift.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <thread>
+#include <visualizer.h>
 
 int main(int argc, char** argv) {
     cv::Mat img = cv::imread(argv[1], cv::IMREAD_COLOR);
