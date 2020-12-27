e422d4a53d8670c0abc3abc30fc9b325698bca76
Xin Jin <xinjin1109@gmail.com>
2020-12-27 17:01:50 +0100

    compute enegry

--- ch7_mean_shift/src/tracking_data_base.cpp
+++ ch7_mean_shift/src/tracking_data_base.cpp
@@ -35,6 +35,9 @@ void TrackerDataBase::update_mass_center() {
     cv::Mat back_proj_weight = compute_back_projection_weight(num_bin, sigma);
 
     cv::Point2f mean_shift = compute_mean_shift(back_proj_weight, sigma);
+    bbox_ = BoundingBox(mean_shift.x - temp_64f_.cols / 2, mean_shift.y - temp_64f_.rows / 2, temp_64f_.cols,
+                        temp_64f_.rows);
+    std::cout << mean_shift.x << " " << mean_shift.y << '\n';
 }
 
 bool TrackerDataBase::is_convergent() {
@@ -94,7 +97,7 @@ std::vector<double> compute_histogram(int num_bin, cv::Mat img, cv::Mat weight) 
         for (int c = 0; c < img.cols; c++) {
             int gray_value = static_cast<int>(img.at<double>(r, c));
             int bin = get_bin(gray_value, width_bin);
-            std::cout << "Gray value is: " << gray_value << ", at Bin " << bin << '\n';
+            // std::cout << "Gray value is: " << gray_value << ", at Bin " << bin << '\n';
             result[bin] += weight.at<double>(r, c);
         }
     }
@@ -152,6 +155,16 @@ cv::Point2f compute_weighted_average(const std::vector<cv::Point>& data, cv::Mat
     return cv::Point2f(sum_x / sum_w, sum_y / sum_w);
 }
 
+double compute_energy(std::vector<double> hist_lhs, std::vector<double> hist_rhs) {
+    assert(hist_lhs.size() == hist_rhs.size());
+    double result = 0.0;
+    for (int i = 0; i < hist_lhs.size(); i++) {
+        result = std::sqrt(hist_lhs[i] * hist_rhs[i]);
+    }
+
+    return result;
+}
+
 cv::Mat TrackerDataBase::compute_back_projection_weight(int num_bin, double sigma) {
     cv::Mat weight = get_gaussian_kernel(this->temp_64f_.cols, this->temp_64f_.rows, sigma);
     std::vector<double> hist_temp = compute_histogram(num_bin, this->temp_64f_, weight);
@@ -159,6 +172,8 @@ cv::Mat TrackerDataBase::compute_back_projection_weight(int num_bin, double sigm
         get_sub_image_from_ul(this->img_64f_, bbox_.top_left().x, bbox_.top_left().y, bbox_.width(), bbox_.height());
     std::vector<double> hist_candidate = compute_histogram(num_bin, candidate, weight);
     cv::Mat back_proj_weight = compute_back_projection(candidate, hist_temp, hist_candidate);
+    // double energy = compute_energy(hist_temp, hist_candidate);
+    // std::cout << "@@@@@@@@@@@ Energy is " << energy << '\n';
     return back_proj_weight;
 }
 
