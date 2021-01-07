70434d89874cc91eec068e9dfa4c1c527248791d
Xin Jin <xinjin1109@gmail.com>
2021-01-07 17:39:09 +0100

    hw version add commits

--- ch8_particle_filter/src/particle_filter.h
+++ ch8_particle_filter/src/particle_filter.h
@@ -77,6 +77,11 @@ class ParticleFilter {
      * @return State
      */
     State compute_mean_state_and_set_observation();
+    /**
+     * @brief Visualiize the particles
+     *
+     * @param frame
+     */
     void visualize(cv::Mat frame);
 
    private:
@@ -88,33 +93,86 @@ class ParticleFilter {
 
 class State {
    public:
+    /**
+     * @brief Construct a new State object
+     *
+     * @param w
+     * @param h
+     * @param x_center
+     * @param y_center
+     */
     State(float w, float h, float x_center, float y_center);
 
+    /**
+     * @brief width of bbox
+     *
+     * @return float
+     */
     float w() const {
         return bbox_.width();
     }
+
+    /**
+     * @brief height of bbox
+     *
+     * @return float
+     */
     float h() const {
         return bbox_.height();
     }
 
+    /**
+     * @brief x center of bbox
+     *
+     * @return float
+     */
     float x_center() const {
         return bbox_.center().x;
     }
 
+    /**
+     * @brief y center of bbox
+     *
+     * @return float
+     */
     float y_center() const {
         return bbox_.center().y;
     }
 
+    /**
+     * @brief center of bbox
+     *
+     * @return cv::Point2f
+     */
     cv::Point2f center() const {
         return bbox_.center();
     }
 
+    /**
+     * @brief resize the bbox
+     *
+     * @param w
+     * @param h
+     */
     void resize(float w, float h) {
         bbox_.resize(w, h);
     }
+
+    /**
+     * @brief size of the bbox
+     *
+     * @return cv::Size2f
+     */
     cv::Size2f size() {
         return bbox_.size();
     }
+
+    /**
+     * @brief Move the bbox with delta x and delta y
+     *
+     * @param delta_x
+     * @param delta_y
+     */
     void move(float delta_x, float delta_y) {
         bbox_.move(delta_x, delta_y);
     }
@@ -123,9 +181,31 @@ class State {
     BoundingBox bbox_;
 };
 struct Particle {
+    /**
+     * @brief Construct a new Particle object
+     *
+     * @param w
+     * @param h
+     * @param x_center
+     * @param y_center
+     * @param weight
+     */
     Particle(float w, float h, float x_center, float y_center, float weight);
+
+    /**
+     * @brief Construct a new Particle object
+     *
+     * @param state
+     * @param weight
+     */
     Particle(State state, double weight);
 
+    /**
+     * @brief Update with motion and noise
+     *
+     * @param delta_motion
+     * @param noise
+     */
     void update_with_motion_and_noise(cv::Vec2f delta_motion, const std::array<double, 4>& noise);
 
     bool bad_ = false;
