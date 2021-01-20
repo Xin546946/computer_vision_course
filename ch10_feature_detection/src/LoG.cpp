ac5272c34019e4aab33ab08bd69a4e490e805d87
Xin Jin <xinjin1109@gmail.com>
2021-01-20 22:17:05 +0100

    hw version

--- ch10_feature_detection/src/LoG.cpp
+++ ch10_feature_detection/src/LoG.cpp
@@ -4,35 +4,7 @@
 void LoG::run() {
     // todo implement LoG
     // step 1: 用Gaussian函数对图像进行平滑，抑制噪声
-    cv::GaussianBlur(img_, result_, param_gaussian_.size_, param_gaussian_.sigma_x_, param_gaussian_.sigma_y_,
-                     cv::BORDER_DEFAULT);
-    cv::imshow("Gaussian Blur", result_);
-    cv::waitKey(0);
-    cv::Mat result_gray;
-    cv::cvtColor(result_, result_gray, cv::COLOR_BGR2GRAY);
-    cv::imshow("gray value", result_gray);
-    cv::waitKey(0);
     // step 2: 对经过平滑的图像使用Laplacian算子
-    cv::Mat lap_dist;
-    cv::Laplacian(result_gray, lap_dist, param_laplacian_.ddepth_, param_laplacian_.kernel_size_,
-                  param_laplacian_.scale_, param_laplacian_.delta_, cv::BORDER_DEFAULT);
-
     // step 3: 二值化图像（一般以0为阈值）
-    cv::convertScaleAbs(lap_dist, lap_dist);
-    // cv::Mat vis_lap = get_float_mat_vis_img(lap_dist);
-    cv::imshow("Laplacian", lap_dist);
-    cv::waitKey(0);
-    cv::Mat thres_dist;
-    cv::threshold(lap_dist, thres_dist, param_thres_.threshold_, param_thres_.max_val_, param_thres_.threshold_type_);
-
-    cv::imshow("threshold to binary img", thres_dist);
-    cv::waitKey(0);
-
     // step 4: 使用形态学方法（膨胀腐蚀）得到图像边缘
-    cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(3, 3), cv::Point(1, 1));
-    cv::Mat erosion_dist, dilation_dist;
-    cv::dilate(thres_dist, dilation_dist, element, cv::Point(-1, -1), 1);   // 膨胀
-    cv::erode(dilation_dist, erosion_dist, element, cv::Point(-1, -1), 2);  // 腐蚀
-    cv::imshow("Result", erosion_dist);
-    cv::waitKey(0);
 }
--- ch10_feature_detection/src/harris_corner.cpp
+++ ch10_feature_detection/src/harris_corner.cpp
@@ -1,71 +1,11 @@
 #include "harris_corner.h"
 #include "opencv_utils.h"
 
-cv::Matx22d compute_M(cv::Mat grad_x, cv::Mat grad_y) {
-    cv::Mat gaussian_kernel = get_gaussian_kernel(7, 3.0);
-
-    grad_x = grad_x.mul(gaussian_kernel);
-    grad_y = grad_y.mul(gaussian_kernel);
-
-    double element1 = grad_x.dot(grad_x);
-    double element4 = grad_y.dot(grad_y);
-    double element23 = grad_x.dot(grad_y);
-
-    cv::Matx22d result(element1, element23, element23, element4);
-
-    return result;
-}
-
 void HarrisCornerDetector::run() {
+    // todo implement harris corner detector
     // step 1: compute x and y derivatives of image
-    cv::Mat img_grad_x, img_grad_y;
-    assert(img_64f_.type() == CV_64FC1);
-    cv::Sobel(img_64f_, img_grad_x, -1, 1, 0, 3);
-    cv::Sobel(img_64f_, img_grad_y, -1, 0, 1, 3);
-
     // step 2: subtract the mean from each image gradient
-    double img_grad_x_mean = cv::mean(img_grad_x)[0];
-    double img_grad_y_mean = cv::mean(img_grad_y)[0];
-    assert(img_grad_x.type() == CV_64F);
-
-    cv::Mat grad_x_zero_mean = img_grad_x - img_grad_x_mean * cv::Mat::ones(img_grad_x.size(), img_grad_x.type());
-    cv::Mat grad_y_zero_mean = img_grad_y - img_grad_y_mean * cv::Mat::ones(img_grad_y.size(), img_grad_y.type());
     // step 3: compute M (covariance) matrix for each pixel
     // step 4: compute eigenvalues and eigenvectors or other criterions such as in slides
-    assert(grad_x_zero_mean.type() == CV_64FC1);
-    cv::Mat responses(img_.size(), CV_64FC1);
-    for (int r = 3; r < img_64f_.rows - 3; r++) {
-        for (int c = 3; c < img_64f_.cols - 3; c++) {
-            cv::Mat grad_x_img_roi = get_sub_image_around(grad_x_zero_mean, c, r, 7, 7);
-            cv::Mat grad_y_img_roi = get_sub_image_around(grad_y_zero_mean, c, r, 7, 7);
-
-            cv::Matx22d M = compute_M(grad_x_img_roi, grad_y_img_roi);
-
-            double k = 0.05;
-            responses.at<double>(r - 3, c - 3) = cv::determinant(M) - k * (cv::trace(M)) * (cv::trace(M));
-        }
-    }
     // step 5: use threshold on eigenvalues to detect corners
-    int window_size = 31;
-    double threshold = 300.0;
-    int border = (window_size - 1) / 2;
-    for (int r = border; r < responses.rows - border; r++) {
-        for (int c = border; c < responses.cols - border; c++) {
-            double response = responses.at<double>(r, c);
-            if (response > threshold) {
-                bool local_max = true;
-                for (int r_win = -border; r_win < border; r_win++) {
-                    for (int c_win = -border; c_win < border; c_win++) {
-                        if (r_win == 0 && c_win == 0) {
-                            continue;
-                        }
-                        local_max = local_max && (response >= responses.at<double>(r + r_win, c + c_win));
-                    }
-                }
-                if (local_max) {
-                    cv::circle(result_, cv::Point(c, r), 5, cv::Scalar(0, 0, 255), 1, cv::LINE_AA);
-                }
-            }
-        }
-    }
 }
