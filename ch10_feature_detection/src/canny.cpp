05b25311d5344d1c32bf23054d06540113d21d29
Xin Jin <xinjin1109@gmail.com>
2021-01-21 21:06:59 +0100

    add todo

--- ch10_feature_detection/src/canny.cpp
+++ ch10_feature_detection/src/canny.cpp
@@ -16,12 +16,13 @@ void Canny::run() {
 
     // non-maxima-suppression
     int nms_size = 3;
-    double threshold_1 = 30.0;
-    double threshold_2 = 220.0;
+    double threshold_1 = 45.0;
+    double threshold_2 = 150.0;
     cv::Mat img_threshold_1(cv::Mat::zeros(img_.size(), CV_64FC1)),
         img_threshold_2(cv::Mat::zeros(img_.size(), CV_64FC1));
     int border = (nms_size - 1) / 2;
     cv::copyMakeBorder(img_64f_, img_64f_, border, border, border, border, cv::BORDER_CONSTANT, 200);
+    // todo nms should be along the gradient
     for (int r = border; r < amplitudes.rows - border; r++) {
         for (int c = border; c < amplitudes.cols - border; c++) {
             double amplitude = amplitudes.at<double>(r, c);
@@ -47,6 +48,7 @@ void Canny::run() {
             // }
         }
     }
+    // std::cout << img_threshold_2 << '\n';
     cv::Mat vis_thres1 = get_float_mat_vis_img(img_threshold_1);
     cv::Mat vis_thres2 = get_float_mat_vis_img(img_threshold_2);
     cv::imshow("threshold 1", img_threshold_1);
@@ -57,7 +59,7 @@ void Canny::run() {
     result_ = img_threshold_2.clone();
     cv::imshow("result_", result_);
     cv::waitKey(0);
-    int window_size_thres = 121;
+    int window_size_thres = 5;
     int border_thres = (window_size_thres - 1) / 2;
     cv::copyMakeBorder(img_threshold_1, img_threshold_1, border_thres, border_thres, border_thres, border_thres,
                        cv::BORDER_CONSTANT, 0);
@@ -65,12 +67,12 @@ void Canny::run() {
                        cv::BORDER_CONSTANT, 0);
     for (int r = border_thres; r < img_threshold_1.rows - border_thres; r++) {
         for (int c = border_thres; c < img_threshold_1.cols - border_thres; c++) {
-            std::cout << img_threshold_2.at<double>(r, c) << '\n';
-            if (img_threshold_2.at<double>(r, c) == 255.0) {
+            // std::cout << img_threshold_2.at<double>(r, c) << '\n';
+            if (double(img_threshold_2.at<double>(r, c)) == 255.0) {
                 for (int r_win = -border_thres; r_win < border_thres; r_win++) {
                     for (int c_win = -border_thres; c_win < border_thres; c_win++) {
-                        if (img_threshold_1.at<double>(r + r_win, c + c_win) == 255.0) {
-                            result_.at<double>(r + r_win - border_thres, c + c_win - border_thres) == 255.0;
+                        if (double(img_threshold_1.at<double>(r + r_win, c + c_win)) == 255.0) {
+                            result_.at<double>(r + r_win - border_thres, c + c_win - border_thres) = 255.0;
                         }
                     }
                 }
