a05ad37c3237d14c013ab564a4ac26174e42cad0
zhaoran <zhaoran.wu1@gmail.com>
2021-01-26 23:51:03 +0100

    avoid duplicate

--- ch12_line_fitting/src/ransac.cpp
+++ ch12_line_fitting/src/ransac.cpp
@@ -3,7 +3,19 @@
 #include <vector>
 
 std::pair<cv::Point2d, cv::Point2d> pick_points(std::vector<cv::Point2d> points, int num) {
-    std::vector<int> points_id = generate_random_data(2, 0, points.size() - 1);
+    std::vector<int> points_id;
+
+    int id = generate_random_data(1, 0, points.size() - 1)[0];
+    points_id.push_back(id);
+
+    while (1) {
+        id = generate_random_data(1, 0, points.size() - 1)[0];
+        if (id != points_id.back()) {
+            points_id.push_back(id);
+            break;
+        }
+    }
+
     return {points[points_id[0]], points[points_id[1]]};
 }
 
@@ -29,15 +41,17 @@ int compute_num_outliers(LineParam line_param, const std::vector<cv::Point2d>& d
 LineParam line_fitting(const std::vector<cv::Point2d>& points) {
     std::vector<cv::Point2d> datas = points;
 
-    int max_iter = 10000;
+    int max_iter = 1e4;
 
     LineParam fitted_line;
     int min_num_outlier = points.size();
     for (int it = 0; it < max_iter; it++) {
         std::pair<cv::Point2d, cv::Point2d> line_points = pick_points(points, 2);
         LineParam line_param = fit_line(line_points.first, line_points.second);
-        double threshold = 2.0;
+
+        double threshold = 5.0;
         int num_outliers = compute_num_outliers(line_param, datas, threshold);
+
         if (num_outliers < min_num_outlier) {
             min_num_outlier = num_outliers;
             fitted_line = line_param;
